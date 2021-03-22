f61d373c0b449ccc61665c79318dd252df54b5f4
huangshuai <huangshuai@sensorsdata.cn>
2021-03-22 23:09:15 +0800

    feat:添加二叉树的遍历

--- 知识点总结/letcode算法题目练习/树🌲/二叉树的后序遍历.js
+++ 知识点总结/letcode算法题目练习/树🌲/二叉树的后序遍历.js
@@ -25,23 +25,23 @@ var postorderTraversal = function (root) {
 
 //迭代
 var postorderTraversal = function (root) {
+  if (!root) return [];
   let res = [];
   let stack = [];
   let prev = null;
-  if (!root) return res;
-  while (stack.length || root) {
-    while (root) {
+  while (stack.length || root) { //当栈中有值 或者root 不为null 继续遍历
+    while (root) {  //先遍历 左子节点 
       stack.push(root);
-      root = root.left;
+      root = root.left; //遍历 左子节点 
     }
-    root = stack.pop();
-    if (!root.right || root.right == prev) {
-      res.push(root.val);
-      prev = root;
-      root = null;
+    root = stack.pop(); //所有的左子节点 入栈后 开始出栈
+    if (!root.right || root.right == prev) { //如果没有右子树 或右子树 之前遍历过 
+      res.push(root.val); //将刚才出栈的节点 val记录下来
+      prev = root; //记录之前遍历的节点
+      root = null; //将节点重置为null
     } else {
-      stack.push(root);
-      root = root.right;
+      stack.push(root); //如果有右子节点说明 还没有遍历完毕 将刚才pop出来的节点 继续入栈 
+      root = root.right; //并且遍历它的右子节点
     }
   }
   return res;
