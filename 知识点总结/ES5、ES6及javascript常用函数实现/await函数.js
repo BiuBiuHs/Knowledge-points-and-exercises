dc3eb5f8468c4cf72f49b54cb3d865885122f846
huangshuai <huangshuai@sensorsdata.com>
2021-04-10 14:13:31 +0800

    fix:添加async 函数的理解

--- 知识点总结/ES5、ES6及javascript常用函数实现/await函数.js
+++ 知识点总结/ES5、ES6及javascript常用函数实现/await函数.js
@@ -14,6 +14,8 @@ function fetchStatus(url){
  * 也就是调用 promise.resolve(xxx)
  * 2. await 后可以跟随任何具有 then 方法的对象 或者说 “thenable"的对象，也就是说非Promise 也可以放在await 后
  * 
+ * 3.***重要 async函数的返回值是 Promise 对象，这比 Generator 函数的返回值是 Iterator 对象方便多了。你可以用then方法指定下一步的操作。
+ *  调用 async 函数后 默认会得到一个 pending 状态的promise 需要使用then 来进行下一步操作 或 获取数据
  */
 
  //eg：执行顺序
