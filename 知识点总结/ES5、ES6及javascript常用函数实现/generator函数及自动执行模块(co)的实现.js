d5db8f159a15bfad0b14b4ee496ae99c2d4785bf
huangshuai <huangshuai@sensorsdata.cn>
2021-05-18 21:11:40 +0800

    feat:添加co

--- 知识点总结/ES5、ES6及javascript常用函数实现/generator函数及自动执行模块(co)的实现.js
+++ 知识点总结/ES5、ES6及javascript常用函数实现/generator函数及自动执行模块(co)的实现.js
@@ -16,7 +16,7 @@ hw.next()// { value: undefined, done: true }
 // 优点： 函数能够暂停执行 
 
 // 实现一个generator函数自动执行器函数  也就是 co模块
-
+//最简单实现
 function run (gen){
   //立即调用 generate 函数 并不会执行 
   // 在后续的next() 中可以传入参数 但是第一次执行时 传入参数无效
@@ -32,3 +32,120 @@ function run (gen){
   
   next()
 }
+
+//丰富版处理 yield后 不是promise的情况
+
+// 第二版
+function run(gen) {
+  var gen = gen();
+
+  return new Promise(function(resolve, reject) {
+
+      function next(data) {
+          try {
+              var result = gen.next(data);
+          } catch (e) {
+              return reject(e);
+          }
+
+          if (result.done) {
+              return resolve(result.value)
+          };
+
+          var value = toPromise(result.value);
+
+          value.then(function(data) {
+              next(data);
+          }, function(e) {
+              reject(e)
+          });
+      }
+
+      next()
+  })
+
+}
+
+function isPromise(obj) {
+  return 'function' == typeof obj.then;
+}
+
+function toPromise(obj) {
+  if (isPromise(obj)) return obj;
+  if ('function' == typeof obj) return thunkToPromise(obj);
+  return obj;
+}
+
+function thunkToPromise(fn) {
+  return new Promise(function(resolve, reject) {
+      fn(function(err, res) {
+          if (err) return reject(err);
+          resolve(res);
+      });
+  });
+}
+
+// module.exports = run;
+
+//最终版 代码优化
+// 第三版
+function run(gen) {
+
+  return new Promise(function(resolve, reject) {
+      if (typeof gen == 'function') gen = gen();
+
+      // 如果 gen 不是一个迭代器
+      if (!gen || typeof gen.next !== 'function') return resolve(gen)
+
+      onFulfilled();
+
+      function onFulfilled(res) {
+          var ret;
+          try {
+              ret = gen.next(res);
+          } catch (e) {
+              return reject(e);
+          }
+          next(ret);
+      }
+
+      function onRejected(err) {
+          var ret;
+          try {
+              ret = gen.throw(err);
+          } catch (e) {
+              return reject(e);
+          }
+          next(ret);
+      }
+
+      function next(ret) {
+          if (ret.done) return resolve(ret.value);
+          var value = toPromise(ret.value);
+          if (value && isPromise(value)) return value.then(onFulfilled, onRejected);
+          return onRejected(new TypeError('You may only yield a function, promise ' +
+              'but the following object was passed: "' + String(ret.value) + '"'));
+      }
+  })
+}
+
+function isPromise(obj) {
+  return 'function' == typeof obj.then;
+}
+
+function toPromise(obj) {
+  if (isPromise(obj)) return obj;
+  if ('function' == typeof obj) return thunkToPromise(obj);
+  return obj;
+}
+
+function thunkToPromise(fn) {
+  return new Promise(function(resolve, reject) {
+      fn(function(err, res) {
+          if (err) return reject(err);
+          resolve(res);
+      });
+  });
+}
+
+// module.exports = run;
