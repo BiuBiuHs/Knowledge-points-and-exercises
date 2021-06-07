1c3f32ffc873a6e47a6aa5d3c82ed8f35f9b6fbb
huangshuai <huangshuai@sensorsdata.cn>
2021-06-07 23:03:50 +0800

    feat:添加知识点

--- 知识点总结/ES5、ES6及javascript常用函数实现/函数柯里化.js
+++ 知识点总结/ES5、ES6及javascript常用函数实现/函数柯里化.js
@@ -1,23 +1,38 @@
 // 第二版
-function sub_curry(fn) {
-    var args = [].slice.call(arguments, 1);
-    return function() {
-        return fn.apply(this, args.concat([].slice.call(arguments)));
-    };
-}
+// function sub_curry(fn) {
+//     var args = [].slice.call(arguments, 1);
+//     return function() {
+//         return fn.apply(this, args.concat([].slice.call(arguments)));
+//     };
+// }
 
-function curry(fn, length) {
+// function curry(fn, length) {
 
-    length = length || fn.length;
+//     length = length || fn.length;
 
-    var slice = Array.prototype.slice;
+//     var slice = Array.prototype.slice;
 
-    return function() {
-        if (arguments.length < length) {
-            var combined = [fn].concat(slice.call(arguments));
-            return curry(sub_curry.apply(this, combined), length - arguments.length);
+//     return function() {
+//         if (arguments.length < length) {
+//             var combined = [fn].concat(slice.call(arguments));
+//             return curry(sub_curry.apply(this, combined), length - arguments.length);
+//         } else {
+//             return fn.apply(this, arguments);
+//         }
+//     };
+// }
+
+
+function curry(fn, ...args) {
+    return function(...innerArgs) {
+        const allArgs = [...args, ...innerArgs];
+        
+        if (fn.length <= allArgs.length) {
+            // 说明已经接受完所有参数，这个时候可以执行了
+            return fn.apply(this, allArgs);
         } else {
-            return fn.apply(this, arguments);
+            // 继续返回函数，收集参数
+            return curry(fn, ...allArgs);
         }
-    };
+    }
 }
