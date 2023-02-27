865b500e382d20c76a01c99651b592f400cb8a4e
huangshuai <huangshuai@sensorsdata.cn>
2023-02-27 10:49:39 +0800

    添加结点遍历函数

--- 函数实现.js
+++ 函数实现.js
@@ -276,7 +276,6 @@ var formatHeader = (headerArr = [],columnFieldIdArr) => {
       const firstColumnVal = nextItem[firstColumnIdKey];
       //如果存在说明处理过表头结构 ，需要取出上一次处理过的结构对表头数据进行合并
       if (fieldValMap[firstColumnVal]) {
-
        const newSubNode =  mergeHeaderItem({
         headerItemObj:headerTree[(fieldValMap[firstColumnVal] - 1)], 
         columnFieldIdArr, 
@@ -416,3 +415,84 @@ DFS(  {
     C:'海淀'
 },['A','B','C',])
 
+
+
+//使用数结构中每个数节点的值 来拼装一直到叶子结点的字符串
+var treeNode = [
+    {
+        title:'华东',
+        fieldId:"A",
+        children:[
+            {
+                title:'天津',
+                fieldId:"B",
+            },
+            {
+                title:'北京',
+                fieldId:"B",
+            },
+        ]
+    },
+    {
+        title:'华南',
+        fieldId:"A",
+        children:[
+            {
+                title:'上海',
+                fieldId:"B",
+            },
+            {
+                title:'深圳',
+                fieldId:"B",
+            },
+        ]
+    },
+]
+
+
+//使用数结构中每个数节点的值 来拼装一直到叶子结点的字符串
+var getNodeStr = (nodeArr,prevstr='')=>{
+    if(!nodeArr) return prevstr
+    return  nodeArr.map(item=>{
+        var {title} = item
+       return  getNodeStr(item?.children,`${prevstr}${prevstr?'*':''}${title}`)
+    })
+}
+
+getNodeStr(treeNode)
+
+
+
+function baseFlatten(array, depth, isStrict,result) {
+    var index = -1,
+        length = array.length;
+    result || (result = []);
+    while (++index < length) {
+      var value = array[index];
+      if (depth > 0 && Object.prototype.toString.call(value)=== '[object Array]' ) {
+        if (depth > 1) {
+          // Recursively flatten arrays (susceptible to call stack limits).
+          baseFlatten(value, depth - 1,  isStrict, result);
+        } else {
+          arrayPush(result, value);
+        }
+      } else if (!isStrict) {
+        result[result.length] = value;
+      }
+    }
+    return result;
+  }
+
+  var deepArr = [
+    [
+        "华东天津",
+        "华东北京"
+    ],
+    [
+        "华南上海",
+        "华南深圳"
+    ]
+]
+baseFlatten(deepArr,Infinity)
+
+  
