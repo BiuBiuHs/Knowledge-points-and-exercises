af46b423b936727bc50661f31dc93b27e69ec263
huangshuai <huangshuai@sensorsdata.com>
2024-08-25 12:30:05 +0800

    添加大鱼吃小鱼算法

--- 知识点总结/algorithms/letcode算法题目练习/栈/大鱼吃小鱼问题.js
+++ 知识点总结/algorithms/letcode算法题目练习/栈/大鱼吃小鱼问题.js
@@ -1,25 +1,55 @@
-// 两个数组一个是鱼的大小，一个是鱼的方向 
+// 两个数组一个是鱼的大小，一个是鱼的方向
 // 每次向 左/右游动一个单位  方向不同且鱼的大小不同时才能吃掉小鱼
 //使用栈来解题
 
-function leaveFish(fishSize,fishDirection) {
-    let stack = []
-    for(var i =0 ;i < fishSize.length; i++) {
-        let hasEat = false
-        const curFishSize = fishSize[i]
-        const curFishDir = fishDirection[i];
-        //与括号匹配消除不同，在此时需要while循环一直匹配到不能吃的鱼为止
-        //用当前元素与栈顶元素 比较，直到满足当前元素被消除 或者栈中没有元素为止
-        while(stack.length && fishDirection[stack.length - 1] !== curFishDir) {
-            if(fishSize[stack.length -1] > curFishSize) {
-                hasEat = true
-                break
-            }
-            stack.pop()
-        }
-        if(!hasEat) {
-            stack.push(i)
-        }
-    }
-    return stack.length
+/**
+ * @param {number[]} sizes - 鱼的大小数组
+ * @param {number[]} directions - 鱼的方向数组 (0 表示向左, 1 表示向右)
+ * @return {number} - 存活的鱼的数量
+ */
+function numFishAlive(sizes, directions) {
+	const stack = []
+
+	for (let i = 0; i < sizes.length; i++) {
+		const currentSize = sizes[i]
+		const currentDirection = directions[i]
+
+		// 如果当前鱼向右游，直接入栈
+		if (currentDirection === 1) {
+			stack.push(i)
+			continue
+		}
+
+		// 当前鱼向左游，需要与栈中向右游的鱼对比
+		while (stack.length > 0) {
+			const topFishIndex = stack[stack.length - 1]
+
+			// 如果栈顶的鱼也是向左游，跳出循环
+			if (directions[topFishIndex] === 0) {
+				break
+			}
+
+			// 比较大小
+			if (sizes[topFishIndex] > currentSize) {
+				// 栈顶的鱼更大，当前鱼被吃掉
+				break
+			} else {
+				// 当前鱼更大，吃掉栈顶的鱼
+				stack.pop()
+			}
+		}
+
+		// 如果栈空了或者栈顶鱼向左游，当前鱼入栈
+		if (stack.length === 0 || directions[stack[stack.length - 1]] === 0) {
+			stack.push(i)
+		}
+	}
+
+	// 栈的大小就是存活鱼的数量
+	return stack.length
 }
+
+// 测试
+const sizes = [4, 2, 5, 3, 1]
+const directions = [1, 1, 0, 0, 0]
+console.log(numFishAlive(sizes, directions)) // 应该输出 3
