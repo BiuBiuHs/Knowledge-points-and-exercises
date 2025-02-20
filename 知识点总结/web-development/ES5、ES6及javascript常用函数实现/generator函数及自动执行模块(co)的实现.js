14d3594913b135684531d1e6da5a283a4019422d
huangshuai1 <huangshuai@sqb-kaci.onaliyun.com>
2025-02-20 10:02:38 +0800

    格式化generator函数文件内容

--- 知识点总结/web-development/ES5、ES6及javascript常用函数实现/generator函数及自动执行模块(co)的实现.js
+++ 知识点总结/web-development/ES5、ES6及javascript常用函数实现/generator函数及自动执行模块(co)的实现.js
@@ -1,23 +1,23 @@
 // Generator函数简介
 // Generator 函数是一个状态机，封装了多个内部状态。执行 Generator 函数会返回一个遍历器对象，可以依次遍历 Generator 函数内部的每一个状态，但是只有调用next方法才会遍历下一个内部状态，所以其实提供了一种可以暂停执行的函数。yield表达式就是暂停标志。
 function* helloWorldGenerator() {
-	yield 'hello'
-	yield 'world'
-	yield 'ending'
+	yield "hello";
+	yield "world";
+	yield "ending";
 }
 
-var hw = helloWorldGenerator()
+var hw = helloWorldGenerator();
 // 调用及运行结果：
-hw.next() // { value: 'hello', done: false }
-hw.next() // { value: 'world', done: false }
-hw.next() // { value: 'ending', done: true }
-hw.next() // { value: undefined, done: true }
+hw.next(); // { value: 'hello', done: false }
+hw.next(); // { value: 'world', done: false }
+hw.next(); // { value: 'ending', done: true }
+hw.next(); // { value: undefined, done: true }
 // 缺点: 每次都要自己手动调用next()
 // 优点： 函数能够暂停执行
 function* helloWorldGenerator() {
-	const x = yield 'hello'
-	const y = yield x + 'world'
-	yield y + 'ending'
+	let x = yield "hello";
+	let y = yield x + "world";
+	yield y + "ending";
 }
 
 // {value: 'helloworld', done: false}
@@ -27,75 +27,75 @@ function* helloWorldGenerator() {
 // 实现一个generator函数自动执行器函数  也就是 co模块
 function co(generatorFunction) {
 	return new Promise((resolve, reject) => {
-		const generator = generatorFunction()
+		const generator = generatorFunction();
 
 		function step(nextResult) {
 			if (nextResult.done) {
-				return resolve(nextResult.value)
+				return resolve(nextResult.value);
 			}
 
 			Promise.resolve(nextResult.value)
 				//每次执行 generator.next() 会得到 yeild 后面的表达式执行后的值
 				// next() 方法可以接受一个参数，这个参数会成为上一个 yield 表达式的返回值。
 				.then((result) => step(generator.next(result)))
-				.catch((error) => reject(error))
+				.catch((error) => reject(error));
 		}
 
-		step(generator.next())
-	})
+		step(generator.next());
+	});
 }
 
 //丰富版处理 yield后 不是promise的情况
 
 // 第二版
 function run(gen) {
-	var gen = gen()
+	var gen = gen();
 
 	return new Promise(function (resolve, reject) {
 		function next(data) {
 			try {
-				var result = gen.next(data)
+				var result = gen.next(data);
 			} catch (e) {
-				return reject(e)
+				return reject(e);
 			}
 
 			if (result.done) {
-				return resolve(result.value)
+				return resolve(result.value);
 			}
 
-			var value = toPromise(result.value)
+			var value = toPromise(result.value);
 
 			value.then(
 				function (data) {
-					next(data)
+					next(data);
 				},
 				function (e) {
-					reject(e)
+					reject(e);
 				}
-			)
+			);
 		}
 
-		next()
-	})
+		next();
+	});
 }
 
 function isPromise(obj) {
-	return 'function' == typeof obj.then
+	return "function" == typeof obj.then;
 }
 
 function toPromise(obj) {
-	if (isPromise(obj)) return obj
-	if ('function' == typeof obj) return thunkToPromise(obj)
-	return obj
+	if (isPromise(obj)) return obj;
+	if ("function" == typeof obj) return thunkToPromise(obj);
+	return obj;
 }
 
 function thunkToPromise(fn) {
 	return new Promise(function (resolve, reject) {
 		fn(function (err, res) {
-			if (err) return reject(err)
-			resolve(res)
-		})
-	})
+			if (err) return reject(err);
+			resolve(res);
+		});
+	});
 }
 
 // module.exports = run;
@@ -104,67 +104,66 @@ function thunkToPromise(fn) {
 // 第三版
 function run(gen) {
 	return new Promise(function (resolve, reject) {
-		if (typeof gen == 'function') gen = gen()
+		if (typeof gen == "function") gen = gen();
 
 		// 如果 gen 不是一个迭代器
-		if (!gen || typeof gen.next !== 'function') return resolve(gen)
+		if (!gen || typeof gen.next !== "function") return resolve(gen);
 
-		onFulfilled()
+		onFulfilled();
 
 		function onFulfilled(res) {
-			var ret
+			var ret;
 			try {
-				ret = gen.next(res)
+				ret = gen.next(res);
 			} catch (e) {
-				return reject(e)
+				return reject(e);
 			}
-			next(ret)
+			next(ret);
 		}
 
 		function onRejected(err) {
-			var ret
+			var ret;
 			try {
-				ret = gen.throw(err)
+				ret = gen.throw(err);
 			} catch (e) {
-				return reject(e)
+				return reject(e);
 			}
-			next(ret)
+			next(ret);
 		}
 
 		function next(ret) {
-			if (ret.done) return resolve(ret.value)
-			var value = toPromise(ret.value)
-			if (value && isPromise(value))
-				return value.then(onFulfilled, onRejected)
+			if (ret.done) return resolve(ret.value);
+			var value = toPromise(ret.value);
+			if (value && isPromise(value)) return value.then(onFulfilled, onRejected);
 			return onRejected(
 				new TypeError(
-					'You may only yield a function, promise ' +
+					"You may only yield a function, promise " +
 						'but the following object was passed: "' +
 						String(ret.value) +
 						'"'
 				)
-			)
+			);
 		}
-	})
+	});
 }
 
 function isPromise(obj) {
-	return 'function' == typeof obj.then
+	return "function" == typeof obj.then;
 }
 
 function toPromise(obj) {
-	if (isPromise(obj)) return obj
-	if ('function' == typeof obj) return thunkToPromise(obj)
-	return obj
+	if (isPromise(obj)) return obj;
+	if ("function" == typeof obj) return thunkToPromise(obj);
+	return obj;
 }
 
 function thunkToPromise(fn) {
 	return new Promise(function (resolve, reject) {
 		fn(function (err, res) {
-			if (err) return reject(err)
-			resolve(res)
-		})
-	})
+			if (err) return reject(err);
+			resolve(res);
+		});
+	});
 }
 
 // module.exports = run;
