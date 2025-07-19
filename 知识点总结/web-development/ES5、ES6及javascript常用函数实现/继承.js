109ab6e622ec310c51c72a85c8d7b7289ca98ff2
huangshuai1 <huangshuai@sqb-kaci.onaliyun.com>
2025-07-19 16:38:45 +0800

    文件格式化

--- 知识点总结/web-development/ES5、ES6及javascript常用函数实现/继承.js
+++ 知识点总结/web-development/ES5、ES6及javascript常用函数实现/继承.js
@@ -5,19 +5,19 @@
 
 //父类型
 function Person(name, age) {
-	;(this.name = name), (this.age = age), (this.play = [1, 2, 3])
-	this.setName = function () {}
+	(this.name = name), (this.age = age), (this.play = [1, 2, 3]);
+	this.setName = function () {};
 }
-Person.prototype.setAge = function () {}
+Person.prototype.setAge = function () {};
 //子类型
 function Student(price) {
-	this.price = price
-	this.setScore = function () {}
+	this.price = price;
+	this.setScore = function () {};
 }
-Student.prototype = new Person() // 子类型的原型为父类型的一个实例对象
-var s1 = new Student(15000)
-var s2 = new Student(14000)
-console.log(s1, s2)
+Student.prototype = new Person(); // 子类型的原型为父类型的一个实例对象
+var s1 = new Student(15000);
+var s2 = new Student(14000);
+console.log(s1, s2);
 
 // 特点
 // 父类新增原型方法/原型属性，子类都能访问到
@@ -32,20 +32,20 @@ console.log(s1, s2)
 // 2.借用构造函数继承
 // 这种方式关键在于:在子类型构造函数中通用call()调用父类型构造函数
 function Person(name, age) {
-	this.name = name
-	this.age = age
+	this.name = name;
+	this.age = age;
 	this.setName = function (newName) {
-		this.name = newName
-	}
+		this.name = newName;
+	};
 }
-Person.prototype.setAge = function () {}
+Person.prototype.setAge = function () {};
 function Student(name, age, price) {
-	Person.call(this, name, age) // 相当于: this.Person(name, age)
+	Person.call(this, name, age); // 相当于: this.Person(name, age)
 	/*this.name = name
     this.age = age*/
-	this.price = price
+	this.price = price;
 }
-var s1 = new Student('Tom', 20, 15000)
+var s1 = new Student("Tom", 20, 15000);
 
 // 特点：
 // 解决了原型链继承中子类实例共享父类引用属性的问题
@@ -60,15 +60,15 @@ var s1 = new Student('Tom', 20, 15000)
 // 3原型链+借用构造函数的组合继承
 // 这种方式关键在于:通过调用父类构造，继承父类的属性并保留传参的优点，然后通过将父类实例作为子类原型，实现函数复用。
 function Person(name, age) {
-	this.name = name
-	this.age = age
+	this.name = name;
+	this.age = age;
 	this.setName = function (newName) {
-		this.name = newName
-	}
+		this.name = newName;
+	};
 }
 Person.prototype.setAge = function () {
-	console.log('111')
-}
+	console.log("111");
+};
 function Student(name, age, price) {
 	//调用了父类的构造函数
 	/**
@@ -76,18 +76,18 @@ function Student(name, age, price) {
 	 * 但是在下方 new student时 就确定了this指向 new 创建的新对象
 	 * 此时 student 的一个实例 来调用 Person构造函数
 	 */
-	Person.call(this, name, age)
-	this.price = price
-	this.setScore = function () {}
+	Person.call(this, name, age);
+	this.price = price;
+	this.setScore = function () {};
 }
-Student.prototype = new Person()
-Student.prototype.constructor = Student //组合继承也是需要修复构造函数指向的
-Student.prototype.sayHello = function () {}
-var s1 = new Student('Tom', 20, 15000)
-var s2 = new Student('Jack', 22, 14000)
-console.log(s1)
-console.log(s1.constructor) //Student
-console.log(p1.constructor) //Person
+Student.prototype = new Person();
+Student.prototype.constructor = Student; //组合继承也是需要修复构造函数指向的
+Student.prototype.sayHello = function () {};
+var s1 = new Student("Tom", 20, 15000);
+var s2 = new Student("Jack", 22, 14000);
+console.log(s1);
+console.log(s1.constructor); //Student
+console.log(p1.constructor); //Person
 
 // 优点：
 
@@ -101,24 +101,24 @@ console.log(p1.constructor) //Person
 
 //4 组合继承优化1
 function Person(name, age) {
-	this.name = name
-	this.age = age
+	this.name = name;
+	this.age = age;
 	this.setName = function (newName) {
-		this.name = newName
-	}
+		this.name = newName;
+	};
 }
 Person.prototype.setAge = function () {
-	console.log('111')
-}
+	console.log("111");
+};
 function Student(name, age, price) {
-	Person.call(this, name, age)
-	this.price = price
-	this.setScore = function () {}
+	Person.call(this, name, age);
+	this.price = price;
+	this.setScore = function () {};
 }
-Student.prototype = Person.prototype
-Student.prototype.sayHello = function () {}
-var s1 = new Student('Tom', 20, 15000)
-console.log(s1)
+Student.prototype = Person.prototype;
+Student.prototype.sayHello = function () {};
+var s1 = new Student("Tom", 20, 15000);
+console.log(s1);
 
 // 优点：
 
@@ -130,19 +130,19 @@ console.log(s1)
 // 5.组合继承优化2
 
 function Person(name, age) {
-	;(this.name = name), (this.age = age)
+	(this.name = name), (this.age = age);
 }
 Person.prototype.setAge = function () {
-	console.log('111')
-}
+	console.log("111");
+};
 function Student(name, age, price) {
-	Person.call(this, name, age)
-	this.price = price
-	this.setScore = function () {}
+	Person.call(this, name, age);
+	this.price = price;
+	this.setScore = function () {};
 }
-Student.prototype = Object.create(Person.prototype) //核心代码
-Student.prototype.constructor = Student //核心代码
-var s1 = new Student('Tom', 20, 15000)
-console.log(s1 instanceof Student, s1 instanceof Person) // true true
-console.log(s1.constructor) //Student
-console.log(s1)
+Student.prototype = Object.create(Person.prototype); //核心代码
+Student.prototype.constructor = Student; //核心代码
+var s1 = new Student("Tom", 20, 15000);
+console.log(s1 instanceof Student, s1 instanceof Person); // true true
+console.log(s1.constructor); //Student
+console.log(s1);
